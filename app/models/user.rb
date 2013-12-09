3b2eb71984e371091e96306676cbec7299e9937e
Carlos Salgado <carlossalgado@MacBook-Pro-de-Carlos-Salgado.local>
2013-12-08 21:15:56 -0500

    Successfull Signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ before_save { self.email = email.downcase }
 
 validates :name, 
 		  presence: true,
-		  length: { maximum: 50 } 
+		    length: { maximum: 50 } 
 
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 validates :email, 
-		  presence: true, 
-		  format: { with: VALID_EMAIL_REGEX },
+		    presence: true, 
+		      format: { with: VALID_EMAIL_REGEX },
 		  uniqueness: { case_sensitive: false }
 
 has_secure_password
